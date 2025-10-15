3bb44b7047410a3f1b3a750ae67fa6313d8c8484
ZarKljh <zarkljh@gmail.com>
2025-10-15 16:12:32 +0900

    ApiSecurityConfig add

---
+++ src/main/java/com/rest/proj/global/security/ApiSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.rest.proj.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ApiSecurityConfig {
+    @Bean
+    SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**")
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .requestMatchers("/api/*/articles").permitAll()
+                                .anyRequest().authenticated()
+                )
+                /*restAPI에서는 jwt방식을 사용하기 때문에 csrf 기능을 꺼놓는다*/
+                .csrf(
+                        csrf -> csrf
+                                .disable()
+                );
+        return http.build();
+    }
+}
