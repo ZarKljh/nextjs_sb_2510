2c90105f3f79862bea2d0e44caea8af6662186d2
ZarKljh <zarkljh@gmail.com>
2025-10-15 16:28:53 +0900

    article mvc create

--- src/main/java/com/rest/proj/ProjApplication.java
+++ src/main/java/com/rest/proj/ProjApplication.java
@@ -2,8 +2,10 @@ package com.rest.proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //BaseEntity 의 @EntityListener 을 작동하게 하는 어노테이션
 public class ProjApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rest/proj/global/jpa/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.rest.proj.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //매개변수가 없는 생성자
+@EntityListeners(AuditingEntityListener.class)
+@ToString //객체를 문자열로 보기좋게 출력하는 어노테이션
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
