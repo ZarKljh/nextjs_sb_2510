c5ff49106a8949ffa56db5dad8cf268dec82ff30
ZarKljh <zarkljh@gmail.com>
2025-10-20 17:33:48 +0900

    frontapp list add

--- frontapp/app/article/[id]/page.tsx
+++ frontapp/app/article/[id]/page.tsx
@@ -1,14 +1,37 @@
 /*다이나믹 라우팅*/
 "use client";
 
-import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ArticleDetail() {
-  const params = useParams();
+  const [articles, setArticles] = useState([]);
 
+  useEffect(() => {
+    getData();
+  }, []);
+  //const params = useParams();
+
+  const getData = async () => {
+    const result = await fetch("http://localhost:8090/api/v1/articles").then(
+      (row) => row.json()
+    );
+    setArticles(result.data.articles);
+    console.log(result.data.articles);
+  };
   return (
     <>
-      <div>게시판 상세 {params.id}</div>
+      <h4>번호 / 제목 / 생성일</h4>
+      {articles.length == 0 ? (
+        <p>현재 게시물이 없습니다.</p>
+      ) : (
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>
+              {article.id}/{article.subject}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
