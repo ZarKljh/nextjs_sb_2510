6605cf1c93bf70b40417f26ec3483043ae25efe5
ZarKljh <zarkljh@gmail.com>
2025-10-24 15:28:36 +0900

    aritlceDto create

--- src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/rest/proj/domain/article/controller/ApiV1ArticleController.java
@@ -1,7 +1,10 @@
 package com.rest.proj.domain.article.controller;
 
+import com.rest.proj.domain.article.dto.ArticleDto;
 import com.rest.proj.domain.article.entity.Article;
 import com.rest.proj.domain.article.service.ArticleService;
+import com.rest.proj.domain.member.entity.Member;
+import com.rest.proj.global.rq.Rq;
 import com.rest.proj.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -20,17 +23,21 @@ import java.util.Optional;
 public class ApiV1ArticleController {
 
     private final ArticleService articleService;
+    private final Rq rq;
 
     @Getter
     @AllArgsConstructor
     public static class ArticlesResponse{
-        private final List<Article> articles;
+        private final List<ArticleDto> articles;
     }
 
 
     @GetMapping
     public RsData<ArticlesResponse> getArticles(){
-        List<Article> articles = articleService.getList();
+        List<ArticleDto> articles = articleService.getList()
+                .stream() // 데이터를 List로 묶어주는 명령어
+                .map(article -> new ArticleDto(article))
+                .toList();
 
         //return articles;
         //return RsData.of("S-1", "성공", articles);
@@ -40,7 +47,7 @@ public class ApiV1ArticleController {
     @Getter
     @AllArgsConstructor
     public static class ArticleResponse{
-        private final Article article;
+        private final ArticleDto article;
     }
 
     @GetMapping("/{id}")
@@ -49,7 +56,7 @@ public class ApiV1ArticleController {
         return articleService.getArticle(id).map(article -> RsData.of(
                 "S-1",
                 "성공",
-                new ArticleResponse(article)
+                new ArticleResponse(new ArticleDto(article))
                 )).orElseGet(()-> RsData.of(
                         "F-1",
                 "%d번 게시물은 존재하지 않습니다.".formatted(id),
@@ -71,13 +78,14 @@ public class ApiV1ArticleController {
 
     @PostMapping
     public RsData<WriteResponse> write(@Valid @RequestBody WriteRequest writeRequest){
-        RsData<Article> writeRs = articleService.create(null,writeRequest.getSubject(), writeRequest.getContent());
+        Member member = rq.getMember();
+        RsData<Article> writeRs = articleService.create(member,writeRequest.getSubject(), writeRequest.getContent());
 
         if(writeRs.isFail()) return (RsData) writeRs;
 
         return RsData.of(writeRs.getResultCode(),
                 writeRs.getMsg(),
-                new WriteResponse(writeRs.getData())
+                new WriteResponse(new ArticleDto(writeRs.getData()))
         );
     }
 
