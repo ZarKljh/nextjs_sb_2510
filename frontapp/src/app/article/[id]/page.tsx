66f44c260b3772998e619ed43784c9249f0b79bc
ZarKljh <zarkljh@gmail.com>
2025-10-27 15:23:13 +0900

    article detail reactQueiry add

--- frontapp/src/app/article/[id]/page.tsx
+++ frontapp/src/app/article/[id]/page.tsx
@@ -4,26 +4,45 @@
 import { useParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import api from "@/app/utils/api";
+import api from "@/src/utils/api";
 
 export default function ArticleDetail() {
   const params = useParams();
-  const [article, setArticle] = useState({});
+  //const [article, setArticle] = useState({});
 
+  const getArticle = async () => {
+    return await api
+      .get(`/articles/${params.id}`)
+      .then((response) => setArticle(response.data.data.article))
+      .catch((err) => console.log(err));
+  };
+  /*
   useEffect(() => {
     api
       .get(`/articles/${params.id}`)
       .then((response) => setArticle(response.data.data.article))
       .catch((err) => console.log(err));
   }, []);
-  return (
-    <>
-      <h4>게시물 상세 {params.id}번</h4>
-      <div>{article.subject}</div>
-      <div>{article.content}</div>
-      <div>{article.createdDate}</div>
-      <div>{article.modifiedDate}</div>
-      <Link href={`/article/${params.id}/edit`}>수정</Link>
-    </>
-  );
+  */
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["article"],
+    queryFn: getArticle,
+  });
+
+  if (error) console.log(error);
+  if (isLoading) <>Loading.......</>;
+
+  if (data) {
+    return (
+      <>
+        <h4>게시물 상세 {params.id}번</h4>
+        <div>{article.subject}</div>
+        <div>{article.content}</div>
+        <div>{article.createdDate}</div>
+        <div>{article.modifiedDate}</div>
+        <Link href={`/article/${params.id}/edit`}>수정</Link>
+      </>
+    );
+  }
 }
--- frontapp/src/app/article/page.tsx
+++ frontapp/src/app/article/page.tsx
@@ -7,7 +7,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import api from "@/app/utils/api";
+import api from "@/src/utils/api";
 
 export default function Article() {
   const [articles, setArticles] = useState([]);
